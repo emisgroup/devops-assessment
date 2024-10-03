89f792f4929b6b8c58a75881f9b678f42a5802ba
Rohit Kondapalli <rohit.kondapalli1@nhs.net>
2024-10-03 18:22:42 +0100

    mend

--- app/src/index.js
+++ app/src/index.js
@@ -1,16 +1,20 @@
-var os = require('os')
-var http = require('http')
-var dotenv = require('dotenv')
+var os = require("os");
+var http = require("http");
+var dotenv = require("dotenv");
 
-const envFile = `.env.${process.env.NODE_ENV || '.env'}`;
+const envFile = `.env.${process.env.NODE_ENV || ".env"}`;
 dotenv.config({ path: envFile });
 
 function handleRequest(req, res) {
-  res.write(`Hi there! I\'m being served from  ${os.hostname()}  Environment: ${process.env.NODE_ENV} Message: ${process.env.MESSAGE}`)
-  res.end()
+  res.write(
+    `Hi there! I\'m being served from  ${os.hostname()}  Environment: ${process.env.NODE_ENV} Message: ${process.env.MESSAGE}`,
+  );
+  res.end();
 }
 
-const server = http.createServer(handleRequest).listen(process.env.PORT||3000)
+const server = http
+  .createServer(handleRequest)
+  .listen(process.env.PORT || 3000);
 
 process.on("SIGTERM", () => {
   console.debug("SIGTERM signal received: closing HTTP server");
